86ddf1b806e5908c07ba74068014daf556190d19
amandasantiagu <amandynhah023@hotmail.com>
2021-08-31 11:53:06 -0300

    add link in button exit

--- my-app/src/components/HeaderMain/HeaderMain.js
+++ my-app/src/components/HeaderMain/HeaderMain.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {  HeaderProf, Pipocando, MainStyle,} from './HeaderMainStyles'
 import logo from '../../assets/images/pipoca.png'
 import {ImExit} from 'react-icons/im'
+import { Link } from 'react-router-dom'
 
 const HeaderStyledComponent = () => {
     return (
@@ -10,7 +11,9 @@ const HeaderStyledComponent = () => {
                 <HeaderProf>
                     <img src={logo} alt="Logo" style={{width: '3.5%', height:'75%', paddingLeft: '0.5rem' }}/>
                     <Pipocando> Hora da Pipoca </Pipocando>
-                    <ImExit size={40} style={{color: '#e48f93' , paddingRight: '3rem',}} />   
+                    <Link to={'/'}>
+                        <ImExit size={40} style={{color: '#e48f93' , paddingRight: '3rem',}} /> 
+                    </Link>
                 </HeaderProf>                
             </MainStyle>
         </>
