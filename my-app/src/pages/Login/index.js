7ad4c53cf721f733ad4529d3b27e3f9c62791c57
amandasantiagu <amandynhah023@hotmail.com>
2021-08-31 23:10:43 -0300

    Campos validados de Input e Register

--- my-app/src/pages/Login/index.js
+++ my-app/src/pages/Login/index.js
@@ -2,9 +2,31 @@ import React from 'react'
 import { ButtonStyle, CardLogin, InputLogin, InputText, LoginStyle, LoginText } from './loginStyles'
 import logo from '../../assets/images/pipoca.png'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 
 const Login = () => {
+
+    const [fieldValuePassword, setFieldValuePassword] = React.useState('');
+    const [fieldValueEmail, setFieldValueEmail] = React.useState('');
+
+    const handleBlurPassword = (e) => {
+        setFieldValuePassword(e.target.value);
+        if (e.target.value === null || e.target.value === undefined || e.target.value === '' ){
+            toast.error('Voce precisa colocar um valor no Password!')  
+        }
+       
+    console.log(fieldValuePassword);
+}
+
+    const handleBlurEmail = (e) => {
+        setFieldValueEmail(e.target.value);
+        if (e.target.value === null || e.target.value === undefined || e.target.value === '' ){
+            toast.error('Voce precisa colocar um valor no Email!') 
+        }
+       
+    console.log(fieldValueEmail);
+}
 
     return (
         <>
@@ -12,10 +34,13 @@ const Login = () => {
             <CardLogin>
                 <img src={logo} alt="Logo" style={{width: '25%', marginBottom: '12px'}}/>
                 <LoginText>Faça seu Login</LoginText>
-                <InputText > Email</InputText>
-                <InputLogin type="text" />
+
+                <InputText > Email </InputText>
+                <InputLogin type="text" onBlur={handleBlurEmail}/>
+
                 <InputText> Senha </InputText>             
-                <InputLogin type="password" />
+                <InputLogin type="password" onBlur={handleBlurPassword}/>
+
                 <Link to={'/home'} style={{width: '100%', display: 'flex', alignItems: 'center',
                  justifyContent: 'center', textDecoration: 'none'}}>
                     <ButtonStyle> ENTRAR </ButtonStyle>
--- my-app/src/pages/Register/index.js
+++ my-app/src/pages/Register/index.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import logo from '../../assets/images/pipoca.png'
 import { ButtonStyle, CardLogin, InputLogin, InputText, LoginStyle, LoginText } from '../Login/loginStyles'
 
 const Register = () => {
+
+    const [fieldValuePassword, setFieldValuePassword] = React.useState('');
+    const [fieldValueEmail, setFieldValueEmail] = React.useState('');
+
+    const handleBlurPassword = (e) => {
+        setFieldValuePassword(e.target.value);
+        if (e.target.value === null || e.target.value === undefined || e.target.value === '' ){
+            toast.error('Voce precisa colocar um Password para se Cadastrar!')  
+        }
+       
+    console.log(fieldValuePassword);
+}
+
+    const handleBlurEmail = (e) => {
+        setFieldValueEmail(e.target.value);
+        if (e.target.value === null || e.target.value === undefined || e.target.value === '' ){
+            toast.error('Voce precisa colocar um Email para se cadastrar!') 
+        }
+       
+    console.log(fieldValueEmail);
+}
+
     return (
         <LoginStyle>
             <CardLogin>
                 <img src={logo} alt="Logo" style={{width: '25%', marginBottom: '12px'}}/>
                 <LoginText>Crie a sua Conta!</LoginText>
-                <InputText> E-mail</InputText>
-                <InputLogin type="text" />
+
+                <InputText> Cadastre seu E-mail</InputText>
+                <InputLogin type="text" onBlur={handleBlurEmail}/>
+
                 <InputText> Cadastre uma senha</InputText>             
-                <InputLogin type="password" />
+                <InputLogin onBlur={handleBlurPassword} type="password" />
+
                 <Link to={'/home'} style={{width: '100%', display: 'flex', alignItems: 'center',
                  justifyContent: 'center', textDecoration: 'none'}}>
                     <ButtonStyle> PRÓXIMO </ButtonStyle>
