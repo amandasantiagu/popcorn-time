33e4dd574bb702656bd4440975f117f26072836a
Margot Machado <margot.mb@outlook.com>
2021-09-01 15:23:17 -0300

    Base Para Home Page do Usuario

---
+++ my-app/src/components/HomeMain/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeMain = () => {
+
+    return (
+        <>
+       <div>
+        <p> -- API DE NOVIDADES DE FILMES -- </p>
+       </div>
+        </>
+    )
+}
+
+export default HomeMain
---
+++ my-app/src/components/HomeRankList/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomeRankList = () => {
+
+    return (
+        <>
+       <div>
+        <p> -- FILMES MAIS FAVORITADOS -- </p>
+       </div>
+        </>
+    )
+}
+
+export default HomeRankList
